d8b398e642fca389b86224b9b44911da27a63e0a
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-03-25 22:38:47 -0400

    styled the navBar

--- src/components/header.js
+++ src/components/header.js
@@ -6,20 +6,22 @@ import headerStyles from "./header.module.scss"
 const Header = () => {
   return (
     <header className={headerStyles.header}>
-      <h1>Igor Tavarez</h1>
+      <h1>
+        <Link to="/" className={headerStyles.title}>Igor Tavarez</Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Home</Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link to="/blog" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Blog</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} >Contact</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>About</Link>
           </li>
         </ul>
       </nav>
