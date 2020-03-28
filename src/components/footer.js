3f0234bd872850a6b6966d932a95b72930d374c5
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-03-27 22:42:57 -0400

    Query the author for the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,20 @@
 import React from "react";
 import footer from "./footer.module.scss";
+import {graphql, useStaticQuery} from "gatsby"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
   return (
     <footer className={footer.container}>
-      <p> Created by Igor Tavarez, &copy; 2020</p>
+      <p> {`Created by ${data.site.siteMetadata.author}, &copy; 2020`}</p>
     </footer>
   )
 }
